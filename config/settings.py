32e99a2ef79e070991e2632d9b9b5751395b822d
alexandr <dmitrymaslov2016@mai.ru>
2023-08-20 18:59:22 +0300

    dockerfile

--- config/settings.py
+++ config/settings.py
@@ -85,12 +85,12 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "ENGINE": "django.db.backends.postgresql",
         "NAME": os.getenv("NAME_DB"),
         "USER": os.getenv("USER_DB"),
         "PASSWORD": os.getenv("PASSWORD"),
         "HOST": os.getenv("HOST_DB"),
-        "PORT": os.getenv("PORT_DB"),
+        # "PORT": os.getenv("PORT_DB"),
     },
 }
 
