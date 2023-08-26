3d8cc5a9a0e47268e47ea2a0f0fb9831b5038b85
alexandr <dmitrymaslov2016@mai.ru>
2023-08-26 16:52:51 +0300

    change gitlab-ci.yml and requirements.txt

--- users/permissions.py
+++ users/permissions.py
@@ -9,6 +9,7 @@ class IsOwner(BasePermission):
 
 class IsModerator(BasePermission):
     """Проверка прав модератора"""
+
     def has_object_permission(self, request, view, obj):
         if request.user.is_superuser:
             return True
