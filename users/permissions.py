18a70801939f5c1ec6c97105547a5e005ac31f3f
alexandr <dmitrymaslov2016@mai.ru>
2023-08-26 13:49:36 +0300

    change gitlab-ci.yml

--- users/permissions.py
+++ users/permissions.py
@@ -2,6 +2,7 @@ from rest_framework.permissions import BasePermission
 
 
 class IsOwner(BasePermission):
+    """Проврка прав на собственность"""
     def has_object_permission(self, request, view, obj):
         return obj.owner == request.user
 
